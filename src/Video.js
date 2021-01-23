97d194323961c00e3c74cb020f7807fbce62ad4a
jplogesh <jplogesh>
2021-01-23 10:59:42 +0530

    Initialise tiktok clone app

---
+++ src/Video.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Video.css";
+
+
+function Video() {
+    return (
+        <div className="video">
+          <h2>this is test </h2> 
+        </div>
+    )
+}
+
+export default Video
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAneEWEhwvL_sZkLd5KuLsHhXzRNi5Z3ys",
+    authDomain: "tictoc-clone-8abbe.firebaseapp.com",
+    projectId: "tictoc-clone-8abbe",
+    storageBucket: "tictoc-clone-8abbe.appspot.com",
+    messagingSenderId: "172765133886",
+    appId: "1:172765133886:web:387d387e1960783989a56f",
+    measurementId: "G-BEXHCH57WP"
+  };
