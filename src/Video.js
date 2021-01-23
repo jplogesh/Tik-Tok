109fa944dc25c779a394ccb1d96670112aa02adc
jplogesh <jplogesh>
2021-01-23 16:57:08 +0530

    add funtions to play or pause while taping on display

--- src/Video.js
+++ src/Video.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import "./Video.css";
 
 
 function Video() {
+   
+  const [playing, setPlaying] =useState(false);
+  const videoRef=useRef(null);
+   
+  const onVideoPress =() => {
+    if (playing) {
+    videoRef.current.pause();
+    setPlaying(false);
+    }
+    else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
     return (
         <div className="video">
           <video className= "video__player"
-          src ="ins.mp4"> </video> 
+          loop
+          onClick={onVideoPress}
+          ref={videoRef}
+          src ="ash1.mp4"> </video> 
           {/*< sidebar /> */}
            {/* <footer/> */}
         </div>
